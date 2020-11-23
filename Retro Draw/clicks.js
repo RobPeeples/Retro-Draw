018e0a4a8124ad118a62736c15f86d16fd7f75a5
VaderRob <69919131+VaderRob@users.noreply.github.com>
2020-11-22 19:26:07 -0500

    Add files via upload

---
+++ Retro Draw/clicks.js
@@ -0,0 +1,36 @@
+// one
+$('.one button').click(function () {
+    $('.one').html("<p>Good Job!</p>");
+  });
+
+// two
+$('.two button').click(function () {
+    $('.two').click(.slideup());
+  });
+
+// three
+$('.three button').click(function () {
+    $('<div = <p>Hey, you clicked this!.</p>');
+    $('.three').append('.three button');
+  });
+
+//four
+$('.one button').click(function () {
+    
+  });
+
+//five
+$('.one button').click(function () {
+    
+  });
+
+//six
+$('.one button').click(function () {
+    
+  });
+
+//seven
+$('.one button').click(function () {
+    
+  });
+
